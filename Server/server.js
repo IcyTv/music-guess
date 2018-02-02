2bf6aa8c5023ca74b4c42d6c3441153bcb2eef2e
IcyTv <mifi2001@gmx.de>
2018-02-02 12:17:00 -0600

    Update

--- Server/server.js
+++ Server/server.js
@@ -1,19 +1,58 @@
+#!/usr/bin/env node
+
 //Custom libs
 const ms = require('./assets/libs/media-server.js');
 const auth = require('./assets/libs/auth-server');
 const hs = require('./assets/libs/highscore-server.js');
+const rsa = require("node-rsa");
+const parser = new (require("argparse").ArgumentParser)({
+  version: 'alpha 0.0.1',
+  addHelp: true,
+  description: 'Server-side for MusicGuess'
+});
 
-let songpath = './assets/music/'
+parser.addArgument(["-f", "--fastboot"], {
+  action: "storeTrue",
+  help: "Enable fastboot"
+});
+
+parser.addArgument(["-m, --media"], {
+  action: "storeTrue",
+  help: "Enable media server"
+});
+
+parser.addArgument(["-a", "--auth"], {
+  action: "storeTrue",
+  help: "Enable Authentication server"
+});
+
+parser.addArgument(["-hi", "--highscore"], {
+  action: "storeTrue",
+  help: "Enable highscore server"
+});
+
+parser.addArgument(["-e", "--every"], {
+  action: "storeTrue",
+  help: "Enable all servers"
+});
+
+let args = parser.parseArgs();
+
+console.log(new rsa().generateKeyPair().encrypt("HELLO"));
+console.log(new rsa().generateKeyPair().sign("HELLO"));
 
 //Media Server
-ms.getMusic(songpath).then((m) => ms.getServer(m));
+if(args.media || args.every){
+  ms.getMusic('./assets/music/', args.fastboot).then((m) => ms.getServer(m));
+}
 
 //Auth Server
-auth.initSql().then(() => {
-  console.log('Initialized sql db');
-  auth.initAuth();
-}).catch((err) => console.log(err));
-
+if(args.auth || args.every){
+  auth.initSql().then(() => {
+    auth.initAuth();
+  }).catch((err) => console.log(err));
+}
 //Highscore Server
-hs.initSql();
-hs.startServer();
+if(args.highscore || args.every){
+  hs.startServer();
+}
